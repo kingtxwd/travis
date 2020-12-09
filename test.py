a20cf215ebea540c8e6c723e702803033f978b9e
Zhaojie Tang <zhaojietang1997@gmail.com>
2020-12-09 12:19:38 -0600

    get file for certain sha

--- test.py
+++ test.py
@@ -3,7 +3,6 @@ import sys
 import csv
 import os
 
-
 def assert_format(file_name):
     with open(file_name,'r') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
@@ -16,19 +15,16 @@ def assert_format(file_name):
                         if not cell.startswith("https://github.com/"): 
                             return False
                     if j == 3:
+
                         if "#" in cell :
+ 
                             return False
                     if j == 4:
-                        if cell == "" or cell == "UD" or cell == "NI" or cell == "NDOI" or cell == "NDOD" or cell == "NOD" or cell == "ID" or cell == "OD-Vic" or cell == "OD-Brit" or cell == "OD":
-                            continue
-                        else :
-                            print(4)
+                        
+                        if not(cell == "" or cell == "UD" or cell == "NI" or cell == "NDOI" or cell == "NDOD" or cell == "NOD" or cell == "ID" or cell == "OD-Vic" or cell == "OD-Brit" or cell == "OD"):
                             return False
                     if j == 5:
-                        if cell == "" or cell == "Blank" or cell == "Opened" or cell == "Accepted" or cell == "InspiredAFix" or cell == "DeveloperFixed" or cell == "Deleted" or cell == "Rejected" or cell == "Skipped":
-                            continue
-                        else :
-                            print(5)
+                        if not (cell == "" or cell == "Blank" or cell == "Opened" or cell == "Accepted" or cell == "InspiredAFix" or cell == "DeveloperFixed" or cell == "Deleted" or cell == "Rejected" or cell == "Skipped") :
                             return False
                     j = j + 1
                 if j > 10:
@@ -37,10 +33,27 @@ def assert_format(file_name):
             i= i + 1
     return True
 
-
+def assert_content(file_name):
+    with open(file_name,'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        i = 0     
+        for row in reader:
+            if i != 0:
+                j = 0
+                sha = ""
+                project = ""
+                for cell in row:
+                    if j == 0 :
+                        project = cell
+                    if j == 1 :
+                        sha = cell
+                    j = j + 1
+                command = "wget "+ project + "/archive/" + sha + ".zip"
+            i= i + 1
+    return True
 
 if assert_format("FlakyTestCharacteristics.csv"):
-    print(True)
-
+    if assert_content("FlakyTestCharacteristics.csv"):
+        print(True)
 else:
     print(False)
