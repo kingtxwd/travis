966fa53603143cccea38e3600bc1946ca30cfc43
Zhaojie Tang <zhaojietang1997@gmail.com>
2020-12-11 04:39:21 -0600

    add semantic check

--- test.py
+++ test.py
@@ -21,10 +21,10 @@ def assert_format(file_name):
                             return False
                     if j == 4:
                         
-                        if not(cell == "" or cell == "UD" or cell == "NI" or cell == "NDOI" or cell == "NDOD" or cell == "NOD" or cell == "ID" or cell == "OD-Vic" or cell == "OD-Brit" or cell == "OD"):
+                        if not(cell in ["", "UD", "NI", "NDOI", "NDOD", "NOD", "ID", "OD-Vic", "OD-Brit", "OD"]):
                             return False
                     if j == 5:
-                        if not (cell == "" or cell == "Blank" or cell == "Opened" or cell == "Accepted" or cell == "InspiredAFix" or cell == "DeveloperFixed" or cell == "Deleted" or cell == "Rejected" or cell == "Skipped") :
+                        if not (cell in ["", "Blank", "Opened", "Accepted", "InspiredAFix", "DeveloperFixed", "Deleted", "Rejected", "Skipped"]) :
                             return False
                     j = j + 1
                 if j > 10:
@@ -37,18 +37,45 @@ def assert_content(file_name):
     with open(file_name,'r') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
         i = 0     
+        downloaded = {}
         for row in reader:
             if i != 0:
                 j = 0
-                sha = ""
-                project = ""
+                sha = row[1]
+                project = row[0]
+                module = row [2]
+                testname = row[3]
+                projectname = row[0].split('/')[-1]
+                """
                 for cell in row:
                     if j == 0 :
                         project = cell
                     if j == 1 :
                         sha = cell
+                    if j == 2 :
+                        module = cell
+                    if j == 3 : 
                     j = j + 1
-                command = "wget "+ project + "/archive/" + sha + ".zip"
+                """
+                key = project + sha
+                dir = sha + "/" + projectname + "-" + sha
+                if key not in downloaded:
+                    command = "wget -N "+ project + "/archive/" + sha + ".zip -O  "+sha+".zip"
+                    zip = os.system(command)
+                    file = os.system("unzip -o "+sha+".zip -d "+sha+"/")
+                    downloaded[key] = 1
+                    # with open(dir + "/pom.xml") as f:
+                    #     s = f.read()
+                    #     if "</plugins>" not in s:
+                    #         pass
+                    #     else:
+                    #         with open(dir + "/pom.xml", 'w') as f:
+                    #             s = s.replace("</plugins>", " <plugin> <groupId>edu.illinois</groupId> <artifactId>nondex-maven-plugin</artifactId> <version>1.1.2</version>  </plugin>  </plugins>")
+                    #             f.write(s)
+                os.system("pwd && cd " + dir + " && mvn install -DskipTests -am " + ("-pl " + module if module != "" else ""))
+                return_value = os.system("mvn edu.illinois:nondex-maven-plugin:1.1.2:nondex -Dtest=" + testname + " -f " + dir  + "/pom.xml")
+                os.system("cd ../../")
+                print(return_value)
             i= i + 1
     return True
 
